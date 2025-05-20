528960b0ed3d7a8a282c7892eaeee10cef9181e0
Ben <brbaker4@byu.edu>
2025-05-20 13:37:52 -0600

    Added logic to MemoryGameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,8 +1,15 @@
 package dataaccess;
 import chess.ChessGame;
+import model.GameData;
+
+import java.util.HashMap;
 
 public interface GameDAO {
 
-    ChessGame createGame() throws DataAccessException;
+    void createGame(String username, ChessGame.TeamColor color, String gameName)
+            throws DataAccessException;
+    GameData getGame(String gameID) throws DataAccessException;
+    HashMap<Integer, GameData> listGames() throws DataAccessException;
+    void updateGame(String gameID, ChessGame.TeamColor color, String username) throws DataAccessException;
     void clear() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,17 +1,55 @@
 package dataaccess;
 
 import chess.ChessGame;
+import model.GameData;
 
 import java.util.HashMap;
 
 public class MemoryGameDAO implements GameDAO{
 
     private int nextId = 1;
-    private final HashMap<Integer, String> games = new HashMap<>();
+    private final HashMap<Integer, GameData> games = new HashMap<>();
 
     @Override
-    public ChessGame createGame() throws DataAccessException {
-        return new ChessGame();
+    public void createGame(String username, ChessGame.TeamColor color, String gameName) throws DataAccessException {
+        int id = nextId++;
+        GameData game;
+        if (color == ChessGame.TeamColor.WHITE) {
+            game = new GameData(id, username, null, gameName, new ChessGame());
+        }
+        else {
+            game = new GameData(id, null, username, gameName, new ChessGame());
+        }
+        games.put(id, game);
+    }
+
+    @Override
+    public GameData getGame(String gameID) throws DataAccessException {
+        return games.get(Integer.parseInt(gameID));
+    }
+
+    @Override
+    public HashMap<Integer, GameData> listGames() throws DataAccessException {
+        return games;
+    }
+
+    @Override
+    public void updateGame(String gameID, ChessGame.TeamColor color, String username) throws DataAccessException {
+        GameData game = getGame(gameID);
+        if (color == ChessGame.TeamColor.WHITE) {
+            if (game.whiteUsername() == null) {
+                GameData updated = new GameData(game.gameID(), username, game.blackUsername(),
+                        game.gameName(), game.game());
+                games.put(game.gameID(), updated);
+            }
+        }
+        else if (color == ChessGame.TeamColor.BLACK) {
+            if (game.blackUsername() == null) {
+                GameData updated = new GameData(game.gameID(), game.whiteUsername(), username,
+                        game.gameName(), game.game());
+                games.put(game.gameID(), updated);
+            }
+        }
     }
 
     @Override
