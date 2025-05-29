25325db4323b15143a1b62bb563dc57caf9659a6
Ben <brbaker4@byu.edu>
2025-05-29 14:28:46 -0600

    Setup ServerFacade.java and started writing unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,51 @@
 package client;
 
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
-
+import server.ServerFacade;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + Integer.toString(port));
     }
 
     @AfterAll
     static void stopServer() {
+        try {
+            facade.clear();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         server.stop();
     }
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerPositive() throws Exception {
+        UserData data = new UserData("user", "pass", "mail");
+        var authData = facade.register(data);
+        Assertions.assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    void registerNegative() throws Exception {
+        UserData data = new UserData(null, "pass", "mail");
+        Assertions.assertThrows(Exception.class, () -> facade.register(data));
+    }
+
+    @Test
+    void clearPositive() throws Exception {
+        Assertions.assertDoesNotThrow(() -> facade.clear());
     }
 
 }
---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,87 @@
+package server;
+
+import com.google.gson.Gson;
+import model.AuthData;
+import model.UserData;
+
+import java.io.*;
+import java.net.*;
+
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public AuthData register(UserData data) throws Exception {
+        var path = "/user";
+        return this.makeRequest("POST", path, data, AuthData.class);
+    }
+
+    public void clear() throws Exception {
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, Exception {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw new Exception("Error");
+                }
+            }
+
+            throw new Exception("Error");
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
+}
