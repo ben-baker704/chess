b4f958ee7b44d79659629f05018ff4ce455c76bb
Ben <brbaker4@byu.edu>
2025-06-03 15:11:56 -0600

    Got a working terminal

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -3,5 +3,35 @@ import java.util.Scanner;
 
 public class Repl {
 
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to the chess server.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + EscapeSequences.SET_TEXT_COLOR_BLACK + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN);
+    }
 
 }
