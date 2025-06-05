501aff45bcb01c4a9f27cfef96c8fe319bf0fb12
Ben <brbaker4@byu.edu>
2025-06-05 15:27:40 -0600

    Setting up websocket files

---
+++ shared/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,44 @@
+package websocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.server.Authentication;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+import java.io.IOException;
+
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws Exception {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connect(command, session);
+            case MAKE_MOVE -> makeMove(command);
+            case LEAVE -> leave(command);
+            case RESIGN -> resign(command);
+        }
+    }
+
+    private void connect(UserGameCommand command, Session session) throws Exception {
+
+    }
+
+    private void makeMove(UserGameCommand command) throws Exception {
+
+    }
+
+    private void leave(UserGameCommand command) throws Exception {
+
+    }
+
+    private void resign(UserGameCommand command) throws Exception {
+
+    }
+}
