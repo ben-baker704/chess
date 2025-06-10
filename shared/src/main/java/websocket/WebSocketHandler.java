b8fa5962238cb8fdc77802b55805c4769af4caca
Ben <brbaker4@byu.edu>
2025-06-10 13:57:04 -0600

    Implemented connect in WebSocketHandler

--- shared/src/main/java/websocket/WebSocketHandler.java
+++ shared/src/main/java/websocket/WebSocketHandler.java
@@ -1,11 +1,16 @@
 package websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
 import org.eclipse.jetty.server.Authentication;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.commands.UserGameCommand;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 import java.io.IOException;
 
@@ -14,6 +19,13 @@ import java.io.IOException;
 public class WebSocketHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public WebSocketHandler(AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
@@ -27,8 +39,18 @@ public class WebSocketHandler {
     }
 
     private void connect(UserGameCommand command, Session session) throws Exception {
-        // VALIDATE auth and existence of game
+        String user = authDAO.getUsername(command.getAuthToken());
+        int gameID = command.getGameID();
+        connections.add(gameID, user, session);
+        ChessGame game = gameDAO.getGame(Integer.toString(gameID)).game();
+        if (game == null) {
+            throw new Exception("Error: game does not exist");
+        }
 
+        ServerMessage loadGame = new LoadGameMessage(game);
+        connections.sendMessageTo(gameID, user, new Gson().toJson(loadGame));
+        ServerMessage notification = new NotificationMessage(user + "joined game");
+        connections.broadcast(gameID, user, new Gson().toJson(loadGame));
     }
 
     private void makeMove(UserGameCommand command) throws Exception {
