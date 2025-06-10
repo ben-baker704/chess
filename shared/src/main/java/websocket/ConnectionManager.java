e2289506f93e66a146e1481009e4283b395190e3
Ben <brbaker4@byu.edu>
2025-06-10 13:01:50 -0600

    Updated add and remove for ConnectionManager

--- shared/src/main/java/websocket/ConnectionManager.java
+++ shared/src/main/java/websocket/ConnectionManager.java
@@ -10,15 +10,24 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, ConcurrentHashMap<String, Connection>>
+            connections = new ConcurrentHashMap<>();
 
-    public void add(String visitorName, Session session) {
+    public void add(int gameID, String visitorName, Session session) {
+        connections.putIfAbsent(gameID, new ConcurrentHashMap<>());
         var connection = new Connection(visitorName, session);
-        connections.put(visitorName, connection);
+        connections.get(gameID).put(visitorName, connection);
+
     }
 
-    public void remove(String visitorName) {
-        connections.remove(visitorName);
+    public void remove(int gameID, String visitorName) {
+        var game = connections.get(gameID);
+        if (game != null) {
+            game.remove(visitorName);
+            if (game.isEmpty()) {
+                connections.remove(gameID);
+            }
+        }
     }
 
 //    public void broadcast(String excludeVisitorName, Notification notification) throws IOException {
